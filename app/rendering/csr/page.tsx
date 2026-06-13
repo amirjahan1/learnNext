d7921ef402763f040e56c19b03d13892241486cc
amir jahan <amirjahan454@gmail.com>
2026-06-13 15:01:20 -0700

    Feature::-> explain ISR && CSR

---
+++ app/rendering/csr/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+function CSRPage(){
+
+    return  (<>
+        <div>
+            <h1>CSR - Client Side Rendering</h1>
+            
+        </div>
+    </>)
+}
+export default CSRPage
