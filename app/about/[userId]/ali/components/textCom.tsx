7bfc0594f9213acd98f6d48e995e94323cbdf20d
amir jahan <amirjahan454@gmail.com>
2026-06-13 13:40:48 -0700

    Feature::->routing param

--- app/about/[userId]/ali/components/textCom.tsx
+++ app/about/[userId]/ali/components/textCom.tsx
@@ -1,15 +1,23 @@
 'use client'
-
+import { usePathname,useRouter } from 'next/navigation'
 function TextCom({children}: any) {
+    const pathname = usePathname()
+    const router = useRouter()
     return(<>
-        <h1 className="bg-blue-300 text-3xl" 
+        <h1 className={` text-3xl ${pathname === '/about/[userId]/ali' ? "bg-blue-300" : "bg-green-300"} `}
         onClick={()=>{
-            console.log("this is for me")
-            alert("heymaster")
+            console.log(pathname)
+             router.push("/about/")
+            //  router.back("/about/")
+            // router.forward()
+            // router.replace()
+
         }}
         >
             {children}
         </h1>
     </>)
 }
+
+
 export default TextCom
